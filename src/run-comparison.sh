cebeafd1d13fa91163f9e4ed7c84da96eeeba540
Matej Drame <matej.drame@gmail.com>
2011-12-23 18:23:15 +0100

    Cleaned up spectral clustering

--- src/run-comparison.sh
+++ src/run-comparison.sh
@@ -1,2 +1,2 @@
 echo "Running spectral clustering on red-blue-clusters.tab..."
-python spectral.py ../datasets/red-blue-clusters.tab
+python spectral.py ../datasets/red-blue-clusters.tab 3
--- src/spectral.py
+++ src/spectral.py
@@ -5,20 +5,35 @@ from utils import data_to_na, parse_tab
 import numpy
 from sklearn.cluster import SpectralClustering
 from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.metrics import adjusted_rand_score, homogeneity_completeness_v_measure
 import pylab
 
-if len(sys.argv) == 1:
-    sys.exit('Usage: python spectral-example.py dataset')
+if len(sys.argv) < 3:
+    sys.exit('Usage: python spectral-example.py dataset k')
 
-examples = data_to_na(parse_tab(sys.argv[1]))
+## Data preprocessing
+data = parse_tab(sys.argv[1])
+k = int(sys.argv[2])
+classes = [example[-1] for example in data]
+
+examples = data_to_na(data)
 distances = euclidean_distances(examples, examples)
 # Apply gaussian kernel as suggested in the documentation:
 gamma = 0.5 # == 1 / num_features (heuristic)
-similarity_matrix = numpy.exp(-distances**2 * gamma)
-sc = SpectralClustering(k=3, random_state=0)
+similarity_matrix = numpy.exp(-distances * gamma)
+
+## Clustering
+sc = SpectralClustering(k=k, random_state=0)
 sc.fit(similarity_matrix)
 
-labels = [label*1. for label in sc.labels_]
+## Performance evaluation
+ari = adjusted_rand_score(sc.labels_, classes)
+homogeneity, completeness, v_measure = homogeneity_completeness_v_measure(sc.labels_, classes)
+print('ARI: {0}'.format(ari))
+print('Homogeneity: {0}'.format(homogeneity))
+print('Completeness: {0}'.format(completeness))
+print('V-measure: {0}'.format(v_measure))
+
 pylab.figure(1)
-pylab.scatter(examples.T[0], examples.T[1], c=labels)
+pylab.scatter(examples.T[0], examples.T[1], c=sc.labels_)
 pylab.show()
