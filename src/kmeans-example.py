999ac6bd5aaa503bd18e5239053f0ec4f64eccb8
Matej Drame <matej.drame@gmail.com>
2011-12-23 13:20:55 +0100

    Transformed kmeans example into color quantization for now, added a photo of china wall

--- src/kmeans-example.py
+++ src/kmeans-example.py
@@ -1,22 +1,66 @@
 #!/usr/bin/env python
+# encoding: utf-8
 import numpy
 from sklearn.cluster import KMeans
+from sklearn.metrics import euclidean_distances
 from sklearn.utils import shuffle
 from scipy.misc import imread
 import pylab
 
+# Reference:
+# http://scikit-learn.org/stable/auto_examples/cluster/plot_color_quantization.html#example-cluster-plot-color-quantization-py
+
+# Photo from (creative commons)
+# http://www.flickr.com/photos/savannahgrandfather/3376760296/
+
+def rgb_to_ycbcr(img):
+    """YCbCr conversion. Pixel values should be 0-255."""
+    height, width = img.shape
+    ycbcr = numpy.zeros((height, width, 3), dtype=numpy.float)
+    ycbcr[:,:,0] = 16 +  img[:,:,0]*(65.481) +  img[:,:,1]*(128.553) + img[:,:,2]*(24.966)
+    ycbcr[:,:,1] = 128 + img[:,:,0]*(-37.797) + img[:,:,1]*(-74.203) + img[:,:,2]*(112.0)
+    ycbcr[:,:,2] = 128 + img[:,:,0]*(112.0) +   img[:,:,1]*(-93.786) + img[:,:,2]*(-18.214)
+    return ycbcr
+
 if __name__ == '__main__':
-    barn_full_img = imread('../presentation/YCbCr-separation.jpg')
-    height = barn_full_img.shape[0] / 4
-    img = barn_full_img[:height,:,:]
-    width = img.shape[1]
-    bw = numpy.zeros((height, width), dtype=numpy.float)
-    cb = numpy.zeros((height, width), dtype=numpy.float)
-    cr = numpy.zeros((height, width), dtype=numpy.float)
-    # YCbCr conversion
-    bw[:,:] = 16 + img[:,:,0]*65.481 + img[:,:,1]*128.553 + img[:,:,2]*24.966
-    cb[:,:] = 128 + img[:,:,0]*(-37.797) + img[:,:,1]*(-74.203) + img[:,:,2]*112.0
-    cr[:,:] = 128 + img[:,:,0]*112.0 + img[:,:,1]*(-93.786) + img[:,:,2]*(-18.214)
+    num_colors = 64
+
+    img = imread('../presentation/china-wall.jpg')
+    img = numpy.array(img, dtype=numpy.float32) / 255
+    width, height, d = img.shape
+    img_array = numpy.reshape(img, (width * height, d))
+    sample = shuffle(img_array, random_state=0)[:1000]
+    kmeans = KMeans(k=num_colors, random_state=0)
+    kmeans.fit(sample)
+    codebook = kmeans.cluster_centers_
+    labels = kmeans.predict(img_array)
+    compressed_array = numpy.array([codebook[labels[index]] for index in range(width*height)])
+    compressed = numpy.reshape(compressed_array, (width, height, d))
+
+    codebook_random = shuffle(img_array, random_state=0)[:num_colors+1]
+    distances = euclidean_distances(codebook_random, img_array, squared=True)
+    labels_random = distances.argmin(axis=0)
+    compressed_array_random = numpy.array([codebook_random[labels_random[index]] for index in range(width*height)])
+    compressed_random = numpy.reshape(compressed_array_random, (width, height, d))
+
+    pylab.figure(1)
+    pylab.clf()
+    pylab.imshow(img)
+
+    pylab.figure(2)
+    pylab.clf()
+    pylab.imshow(compressed)
+
+    pylab.figure(3)
+    pylab.clf()
+    pylab.imshow(compressed_random)
+
+    pylab.show()
+
+    import sys
+    sys.exit(0)
+    #barn_full_img = imread('../presentation/YCbCr-separation.jpg')
+    #height = barn_full_img.shape[0] / 4
     # Display all three channels
     # TODO: use appropriate colors instead of gray levels only
     #pylab.figure(1)
@@ -41,7 +85,6 @@ if __name__ == '__main__':
             histogram[cb[i, j], cr[i, j]] += 1
     # Most used colors (clusters are not very distinct in this example):
     # TODO: maybe do another example like
-    # http://scikit-learn.org/stable/auto_examples/cluster/plot_color_quantization.html#example-cluster-plot-color-quantization-py
     pylab.figure(4)
     pylab.clf()
     plot = pylab.imshow(histogram>10000)
