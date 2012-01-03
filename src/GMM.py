d54383af511598e6477aa07dde6e0638d6c9bb42
kekec pasteta <pb2979@student.uni-lj.si>
2012-01-03 10:17:37 +0100

    Changed GMM to fit other algorithms

--- src/GMM.py
+++ src/GMM.py
@@ -1,32 +1,38 @@
 import drawing as draw
 draw.savedImages = {}
 
+import sys
 import numpy as np
-import pylab as pl
+import pylab
+import os
 from sklearn import mixture
+from sklearn.metrics import adjusted_rand_score, homogeneity_completeness_v_measure
+from utils import data_to_na, parse_tab
 
-execfile("data.py")
-#call reloadData() if needed
 
-
-def GMM(data, clusters, dataNr = 0):
-	clf = mixture.GMM(n_components=clusters) #, cvtype='full')
-	clf.fit(data)
+if len(sys.argv) < 3:
+    sys.exit('Usage: python kmeans.py dataset k')
 	
-	x = np.linspace(min([x[0] for x in data]), max([x[0] for x in data]))
-	y = np.linspace(min([y[1] for y in data]), max([y[1] for y in data]))
-	X, Y = np.meshgrid(x, y)
-	XX = np.c_[X.ravel(), Y.ravel()]
+## Data preprocessing
+data = parse_tab(sys.argv[1])
+clusters = int(sys.argv[2])
+classes = [example[-1] for example in data]
+examples = data_to_na(data)
 
-	Z = np.log(-clf.eval(XX)[0])
-	Z = Z.reshape(X.shape)
+## Clustering
+clf = mixture.GMM(n_components=clusters)
+clf.fit(examples)
+labels = clf.predict(examples)
 
-	pl.contour(X, Y, Z)
+## Performance evaluation
+ari = adjusted_rand_score(labels, classes)
+homogeneity, completeness, v_measure = homogeneity_completeness_v_measure(labels, classes)
+print('ARI: {0}'.format(ari))
+print('Homogeneity: {0}'.format(homogeneity))
+print('Completeness: {0}'.format(completeness))
+print('V-measure: {0}'.format(v_measure))
 
-	pl.scatter(data[:, 0], data[:, 1], .8)
-
-	draw.setImgTitle('GMM_test' + str(dataNr))
-	draw.showImage()
-
-
-test(0, GMM)
+pylab.scatter(examples.T[0], examples.T[1], c=labels)
+print(os.path.splitext(os.path.basename(sys.argv[1]))[0])
+draw.setImgTitle('GMM_' + os.path.splitext(os.path.basename(sys.argv[1]))[0])
+draw.showImage()
